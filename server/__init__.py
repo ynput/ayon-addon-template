6df7d4f10be8580bca60a293986c9c261662d3d8
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-06-27 15:09:19 +0200

    removed unnecessary attributes

--- server/__init__.py
+++ server/__init__.py
@@ -3,13 +3,9 @@ from typing import Type
 from ayon_server.addons import BaseServerAddon
 
 from .settings import MySettings, DEFAULT_VALUES
-from .version import __version__
 
 
 class MyAddon(BaseServerAddon):
-    name = ""
-    title = ""
-    version = __version__
     settings_model: Type[MySettings] = MySettings
 
     async def get_default_settings(self):
